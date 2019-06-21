81d64e0cb5db1cc6c99369876465366315252256
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-06-21 09:34:51 -0400

    add styled-components theme

---
+++ src/lib/Theme.js
@@ -0,0 +1,68 @@
+import { css } from 'styled-components';
+
+const sizes = {
+  smallTablet: 630,
+  mediumTablet: 712,
+  tablet: 880,
+  desktop: 992,
+};
+
+// iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((accumulator, label) => {
+  // use em in breakpoints to work properly cross-browser and support users
+  // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+  const emSize = sizes[label] / 16;
+  return {
+    ...accumulator,
+    [label]: (...args) => css`
+      @media (min-width: ${emSize}em) {
+        ${css(...args)}
+      }
+    `,
+  };
+}, {});
+
+export const colours = {
+  link: '#61DBFF',
+  cta: '#062F70',
+  support: '#FFFFFF',
+  text: 'rgba(0,0,0,0.85)',
+  background: 'rgba(0,0,0,0.02)',
+};
+
+export const fonts = {
+  header: '"Oxygen", "sans-serif"',
+  body: '"Open Sans", "serif"',
+};
+
+export const spacer = '16px';
+
+export const borderRadius = '16px 0px 16px 0px';
+
+export const ratio = {
+  one: '1.618',
+  two: '2.618',
+  three: '4.236',
+};
+
+export const linkStyles = css`
+  color: ${colours.text};
+  text-decoration: none;
+  padding: 0 1px;
+  box-shadow: inset 0 -6px 0 0 ${colours.link};
+  transition: box-shadow 0.3s ease-in;
+
+  &:hover {
+    box-shadow: inset 0 -30px 0 0 ${colours.link};
+  }
+`;
+
+export default {
+  media,
+  colours,
+  fonts,
+  spacer,
+  borderRadius,
+  ratio,
+  linkStyles,
+};
